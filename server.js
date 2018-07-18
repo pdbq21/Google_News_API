69344fe94f6670eb5d2fe237d9168b78f0840171
pdbq21 <pdbq2122@gmail.com>
2018-07-18 05:33:59 +0300

    done: added geoip

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const maxmind = require('maxmind');
 const server = express();
-const {logger} =  require('./logger');
+const {logger} = require('./logger');
 
-const PORT =  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-server.get('/', (req, res)=> {
+function getCountryCode(ip) {
+    let log, ipInfo;
+    if (maxmind.validate(ip)) {
+        log = ip;
+        maxmind.open('./db/geo.mmdb', (err, data) => {
+            const info = data.get(ip);
+            if (!!info) {
+                ipInfo = `<div>IP: ${ip}</div>
+                        <div>City: ${info.city.names.en}</div>
+                        <div>Country: ${info.country.iso_code}</div>
+                        <div>Location: ${info.location}</div>`;
+            } else {
+                log = 'Warning: information about this ip (' + ip + ') not found'
+            }
+        });
+    } else {
+        log = 'Error: Not validate ip: ' + ip;
+    }
+    logger(log, 'ip');
+    return ipInfo || 'not found information';
+}
+
+server.get('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    logger(ip, 'ip');
-    res.send('ip: ' + ip)
+    res.send(getCountryCode(ip))
 });
 
-server.listen(PORT, function(){
-    console.log('server working on port '+ PORT)
+server.listen(PORT, function () {
+    console.log('server working on port ' + PORT)
 });
