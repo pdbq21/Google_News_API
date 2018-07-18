6ffe55a5612fc3bcd5551d5be84ac6b44c8c3cc4
pdbq21 <pdbq2122@gmail.com>
2018-07-18 04:23:50 +0300

    hot-fix: show ip

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const server = express();
 
 server.set('port', process.env.PORT || 3000);
 server.get('/', (req, res)=> {
-    res.send('Test')
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.send('Test: ' + ip)
 });
 
 server.listen(server.get('port'), function(){
